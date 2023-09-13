366fc430ab6c750647dd70fdb98459270dddbdb3
rizava810529 <rizava810529@gmail.com>
2023-09-13 17:35:35 -0500

    checkInView creado

---
+++ config/contenido.php
@@ -0,0 +1,9 @@
+// Comprobamos tenga sesion, si no entonces redirigimos y MATAMOS LA EJECUCION DE LA PAGINA.
+if (isset($_SESSION['usuario'])) {
+	require 'views/contenido.view.php';
+} else {
+	header('Location: login.php');
+	die();
+}
+
+?>
