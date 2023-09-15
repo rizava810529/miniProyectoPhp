2c5d5fa80f68415bb1da4c474b20148cbcee9ab8
rizava810529 <rizava810529@gmail.com>
2023-09-15 12:32:40 -0500

    view personal imfo

---
+++ views/profile/profile.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <!-- Font Awesome -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css"
+    rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/views/profile/profile.css">
+</head>
+<body>
+    <div>
+        <div class="h-100 d-flex flex-column justify-content-center align-items-center ">
+            <h1 class="title">Personal Info</h1>
+            <p class="title1">Some info may be visible to other people</p>
+        </div>
+        <div class="card  d-flex justify-content-center align-items-center">
+            <div class="card-body " style="width: 50%; height: 40%; ">
+                <div>
+                    <div class="d-flex  justify-content-between align-items-start line p-2">
+                        <div>
+                            <h3><P>Profile</P></h3>
+                            <p>Some info may be visible to other people</p>
+                        </div>
+                        <div class="d-flex  align-items-center m-3">
+                            <button type="button" class="btn btn-outline-secondary"                             data-mdb-ripple-color="dark">Edit</button>
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+                <div class="d-flex justify-content-between align-items-center line p-2">
+                    <div>PHOTO</div>
+                    <div> <img src=" " alt="" style="width: 40px; height: 40px; "></div>
+                </div>
+                <div class="d-flex justify-content-between align-items-center line p-2">
+                    <div>NAME</div>
+                    <div>NOMBRE DE BASE DE DATOS</div>
+
+                </div>
+                <div class="d-flex justify-content-between align-items-center line p-2">
+                    <div>BIO</div>
+                    <div>I am a software developer and a big fan of devchallenges... </div>
+                </div>
+                <div class="d-flex justify-content-between align-items-center line p-2">
+                    <div>PHONE</div>
+                    <div>908249274292</div>
+                </div>
+                <div class="d-flex justify-content-between align-items-center line p-2">
+                    <div>EMAIL</div>
+                    <div>xanthe.neal@gmail.com</div>
+                </div>
+                <div class="d-flex justify-content-between align-it5ems-center line p-2">
+                    <div>PASSWORD</div>
+                    <div>++++++++++++++++++</div>
+                </div>
+                
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+        <div class="h-100 d-flex flex-column justify-content-center align-items-center ">
+
+            <div class="">
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+    </div>
+
+
+
+    
+
+
+
+
+
+
+
+
+        <!-- MDB -->
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"
+    ></script>
+</body>
+</html>
